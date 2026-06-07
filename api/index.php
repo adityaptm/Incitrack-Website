74d69189aa7fd7f343556c7a4baf67616aaf9229
aditya <putraadityapratama236@gmail.com>
2026-06-07 13:29:41 +0700

    Fix Laravel Vercel 500 Server Error

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
+// Set directories to /tmp since Vercel's filesystem is read-only
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('VIEW_COMPILED_PATH=/tmp');
+
 // Forward Vercel requests to Laravel's index.php
 require __DIR__ . '/../public/index.php';
